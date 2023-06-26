1d6460b6f0caae82d7fe1c821b39812b81b6aa9a
Josech03 <whitesox3437@gmail.com>
2023-06-25 21:58:47 -0400

    main

--- src/routers/login.js
+++ src/routers/login.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const loginControllers = require("../controllers/LoginController")
+const loginControllers = require("../controllers/LoginController");
+
 router.get("/index", loginControllers.login);
 router.post("/index", loginControllers.auth);
 router.get("/signup", loginControllers.signup);
 router.post("/signup", loginControllers.storeUser);
-router.get("/logout", loginControllers.logout)
+router.get("/logout", loginControllers.logout);
+
 module.exports = router;
